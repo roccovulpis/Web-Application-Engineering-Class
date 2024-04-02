3a53d55b52b515a9be3ad67b91e6ed9f1e518267
Rocco Vulpis <146395748+roccovulpis@users.noreply.github.com>
2024-04-02 15:03:22 -0400

    FIxed Links
    Hovering links now display them as red if not visited and purple if visited.

--- Midterm/script.js
+++ Midterm/script.js
@@ -1,12 +1,19 @@
-document.querySelectorAll('.col a').forEach(function(link) {
+const visitedLinks = document.querySelectorAll('.col a');
+
+
+visitedLinks.forEach(function(link) {
     link.addEventListener('mouseover', function() {
-        this.style.color = 'red'; 
         this.style.background = 'grey'; 
     });
 
     link.addEventListener('mouseout', function() {
-        this.style.color = 'red'; 
         this.style.background = 'transparent'; 
+    });
+
+    link.addEventListener('click', function() {
+        link.style.color = 'magenta'; 
+        link.classList.add = 'visited'; 
+        checkAllLinksVisited();
     });
 
 });
