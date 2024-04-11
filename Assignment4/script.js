e822e0201f4d198dfc4afa4ed68162b58e15c0fb
Rocco Vulpis <146395748+roccovulpis@users.noreply.github.com>
2024-04-11 00:04:43 -0400

    Finished Homepage
    Navbar, card, and dropdowns are now complete. Added an alert message when read me button is clicked.

---
+++ Assignment4/script.js
@@ -0,0 +1,8 @@
+function readMore() {
+    window.alert("You should have the follorwing choices: 1. Image Rollover 2. Image Preview 3. Image Slideshow. Choose the option from the the form below.");
+};
+
+
+function submitImageSelect() {
+
+};
