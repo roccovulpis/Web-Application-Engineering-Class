23f5f8e54542b643a942a2906658b346c93801f9
Rocco Vulpis <146395748+roccovulpis@users.noreply.github.com>
2024-04-11 17:29:59 -0400

    Added Slideshow, rollover, and preview pages

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,8 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('image-options-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var valueSelected = document.getElementById('image-selection').value;
+        var webpageContainer = document.getElementsByClassName('container')[0];
+
+        if (valueSelected === 'Rollover') {
+            window.location.href = "./rollover.html";
+
+        }
+
+        if (valueSelected === 'Preview') {
+            window.location.href = "./preview.html";
+
+        }
+
+        if (valueSelected === 'Slideshow') {
+            window.location.href = "./slideshow.html";
+
+        }
+
+    });
+});
+
+function changePic(imgsrc)
+{
+document.getElementById("pic").src = imgsrc
+}
+
 function readMore() {
-    window.alert("You should have the follorwing choices: 1. Image Rollover 2. Image Preview 3. Image Slideshow. Choose the option from the the form below.");
-};
-
-
-function submitImageSelect() {
-
-};
+    alert("You have the following choices: 1. Image Rollover 2. Image Preview 3. Image Slideshow. Choose the option from the form below.");
+}
