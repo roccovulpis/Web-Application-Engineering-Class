82faa7b5548fbd6c3cb171d622e93383a022296c
Rocco Vulpis <146395748+roccovulpis@users.noreply.github.com>
2024-04-12 17:54:46 -0400

    Fixed slideshow and preview
    Now work as intended

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -32,3 +32,35 @@ document.getElementById("pic").src = imgsrc
 function readMore() {
     alert("You have the following choices: 1. Image Rollover 2. Image Preview 3. Image Slideshow. Choose the option from the form below.");
 }
+
+pics = new Array("/Assignment4/images/07car1.jpg", "/Assignment4/images/2019_genesis_g80_angularfront.jpg", "/Assignment4/images/p_red_car.jpg")
+                        count = 0; picCount = pics.length;
+
+                        function nPic() {
+
+                            count++;
+
+                            if(count < picCount) {
+
+                                document.getElementById("pic").src = pics[count]
+                            }
+                            else {
+                                count = 0
+                                document.getElementById("pic").src = pics[count]
+                            }
+                        }
+
+                        function pPic() {
+
+                            count--
+
+                            if(count > -1) {
+
+                                document.getElementById("pic").src = pics[count]
+                            }
+                            else {
+
+                                count = picCount - 1;
+                                document.getElementById("pic").src = pics[count]
+                            }
+                        }
